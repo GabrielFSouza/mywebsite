da4b9703d191a2cc3f7d866702fee7ff8c58c792
Gabriel F Souza <gabrielferreira717@outlook.com>
2024-05-20 19:20:20 -0300

    Realizando primeiro commit do meu site.

---
+++ assets/js/main.js
@@ -0,0 +1,118 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".stone",
+  `Trabalhei no produto de desenvolvimento de melhorias e sustenação para o sistema de saúde visando a gestão de clínicas e consultórios.`,
+  "Desenvolvedor Pleno (Remoto)",
+  "Stone",
+  "Nov 2023 - Maio 2024 | São Paulo-SP, Brasil"
+);
+
+hoverChangeExperience(
+  ".bellinati",
+  `Desenvolvimento de aplicativos
+  e manutenção para recuperação
+  de crédito para as principais
+  instituições financeiras do
+  Brasil.`,
+  "Desenvolvedor Pleno (Remoto)",
+  "Bellinati Perez",
+  "Dez 2022 - Junho 2023 | Curitiba-PR, Brasil"
+);
+
+hoverChangeExperience(
+  ".adaptive",
+  `Trabalhei com sistemas de
+  ERP para Postos de
+  Combustível, Conveniência e Supermercado para
+  o varejo`,
+  "Desenvolvedor Júnior (Híbrido)",
+  "Adaptive",
+  "Jan 2020 - Out 2021 | Uberlândia-MG, Brasil"
+);
+
+hoverChangeExperience(
+  ".linx",
+  `Trabalhei com sistemas de
+  ERP para Postos de
+  Combustível e Conveniência para
+  o varejo.`,
+  "Suporte ERP (Presencial)",
+  "Linx S.A",
+  "Mar 2019 - Dez 2019 | Uberlândia-MG, Brasil"
+);
+
+hoverChangeExperience(
+  ".paschoalotto",
+  `Desenvolvimento de aplicações e
+  manutenção para recuperação de
+  crédito
+  para o principal financeiro
+  instituições no Brasil.
+  Desenvolvedor nível 4 na esteira, trabalhando
+  nas manutenções, correções e
+  aplicação de melhorias
+  sistêmicas.
+  microserviços, API e
+  formulários (Desktop).`,
+  "Desenvolvedor Pleno (Remoto)",
+  "Paschoalotto",
+  " Dez 2021 - Nov 2022 | Bauru-SP, Brasil"
+);
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
+);
+hoverChangeDescription(
+  ".sass",
+  "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
+);
+hoverChangeDescription(
+  ".react",
+  "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
+);
+hoverChangeDescription(
+  ".next",
+  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
+);
+hoverChangeDescription(
+  ".styled",
+  "styled-components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "Tailwind CSS é um framework CSS que nos fornece classes utilitárias com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".typescript",
+  "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
+);
+hoverChangeDescription(
+  ".radix",
+  "Radix é uma biblioteca que fornece componentes acessíveis e sem estilos para a criação de aplicação em React."
+);
+hoverChangeDescription(
+  ".cypress",
+  "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
+);
+hoverChangeDescription(
+  ".storybook",
+  "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+);
